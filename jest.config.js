ec7e632fe69634d263d7ae00be812b05e962e06a
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-01-25 01:39:37 +0100

    test: updated jest coverage stats

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   collectCoverageFrom: ["src/*.ts"],
   coverageThreshold: {
     global: {
-      branches: 40,
-      functions: 40,
-      lines: 40,
-      statements: 40,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
     },
   },
 }
