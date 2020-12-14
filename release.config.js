7f8e9c49289a930494d229f9b3c785c70a57bc49
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-12-14 14:56:18 +0100

    build: 📦️ semantic release config

--- release.config.js
+++ release.config.js
@@ -12,12 +12,18 @@ module.exports = {
         parserOpts: {
           noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
         },
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
         presetConfig: {
           types: [
             { type: "feat", section: "Features" },
             { type: "fix", section: "Bug Fixes" },
             { type: "chore", section: "Other" },
-            { type: "docs", section: "Other" },
+            { type: "docs", section: "Documentation" },
             { type: "style", section: "Other" },
             { type: "refactor", section: "Other" },
             { type: "perf", section: "Other" },
@@ -28,7 +34,6 @@ module.exports = {
         },
       },
     ],
-    "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/github",
     "@semantic-release/npm",
